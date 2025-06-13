b3103f3daef1030f4a3ba1406eee81fd5f489bc9
MatheusMoura-26 <teu.augusto@gmail.com>
2025-06-13 04:28:59 -0300

    feat(USER_FLOW):  adding user page

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -4,6 +4,7 @@ import { LoginPage } from '../presentation/pages/login';
 import { DashboardPage } from '../presentation/pages/dashboard';
 import { FinancialGoalsPage } from '../presentation/pages/financialGoals';
 import { AccountCardPage } from '../presentation/pages/accountCard';
+import { UserProfilePage } from '../presentation/pages/user';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/financial-goals" element={<FinancialGoalsPage />} />
         <Route path='/acount-user' element={<AccountCardPage />} />
+        <Route path="/user-profile" element={<UserProfilePage />} />
       </Routes>
     </Router>
   );
